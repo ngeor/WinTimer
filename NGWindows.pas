6f6ce7c11838302be115276d3f289773409bab2b
Nikolaos Georgiou <nikolaos.georgiou@gmail.com>
2021-11-01 07:14:27 +0100

    Auto-formatted Pascal files: ConfigureBox.pas, Grafix.pas, NGWindows.pas, WinTimer.lpr, WinTimerCtl.pas

--- NGWindows.pas
+++ NGWindows.pas
@@ -6,25 +6,25 @@ interface
 
 uses Windows, Messages;
 
-function MsgBox(Wnd: HWND; Text, Title: string; Flags: Integer): Integer;
+function MsgBox(Wnd: HWND; Text, Title: string; Flags: integer): integer;
 procedure ErrorDlg(Wnd: HWND; Text: string);
 procedure InfoDlg(Wnd: HWND; Text: string);
 
-function FocusTo(Wnd: HWND; idCtl: Integer): LongInt;
+function FocusTo(Wnd: HWND; idCtl: integer): longint;
 
 function GetWndText(Wnd: HWND): string;
-function GetChildText(Wnd: HWND; idCtl: Integer): string;
+function GetChildText(Wnd: HWND; idCtl: integer): string;
 function SimpleDlgProc(Wnd: HWND; Msg: UINT; wp: WPARAM; lp: LPARAM): BOOL; stdcall;
 
 implementation
 
 resourcestring
   ErrorMsg = 'Error';
-  InfoMsg  = 'Information';
+  InfoMsg = 'Information';
 
-function MsgBox(Wnd: HWND; Text, Title: string; Flags: Integer): Integer;
+function MsgBox(Wnd: HWND; Text, Title: string; Flags: integer): integer;
 begin
-  Result:=MessageBox(Wnd, PChar(Text), PChar(Title), Flags);
+  Result := MessageBox(Wnd, PChar(Text), PChar(Title), Flags);
 end;
 
 procedure ErrorDlg(Wnd: HWND; Text: string);
@@ -37,37 +37,37 @@ begin
   MsgBox(Wnd, Text, InfoMsg, MB_ICONINFORMATION);
 end;
 
-function FocusTo(Wnd: HWND; idCtl: Integer): LongInt;
+function FocusTo(Wnd: HWND; idCtl: integer): longint;
 begin
-  Result:=SendMessage(Wnd, WM_NEXTDLGCTL, GetDlgItem(Wnd, idCtl), 1);
+  Result := SendMessage(Wnd, WM_NEXTDLGCTL, GetDlgItem(Wnd, idCtl), 1);
 end;
 
 function GetWndText(Wnd: HWND): string;
 var
-  Len: Integer;
+  Len: integer;
   Buf: PChar;
 begin
-  Len:=GetWindowTextLength(Wnd) + 1;
+  Len := GetWindowTextLength(Wnd) + 1;
   GetMem(Buf, Len);
   GetWindowText(Wnd, Buf, Len);
-  Result:=Buf;
+  Result := Buf;
   FreeMem(Buf);
 end;
 
-function GetChildText(Wnd: HWND; idCtl: Integer): string;
+function GetChildText(Wnd: HWND; idCtl: integer): string;
 begin
-  Result:=GetWndText(GetDlgItem(Wnd, idCtl));
+  Result := GetWndText(GetDlgItem(Wnd, idCtl));
 end;
 
 function SimpleDlgProc(Wnd: HWND; Msg: UINT; wp: WPARAM; lp: LPARAM): BOOL; stdcall;
 begin
-  Result:= Msg = WM_INITDIALOG;
+  Result := Msg = WM_INITDIALOG;
   case Msg of
     WM_CLOSE: EndDialog(Wnd, 0);
     WM_COMMAND:
       case LOWORD(wp) of
-        IDOK: EndDialog(Wnd, 1);
-        IDCANCEL: EndDialog(Wnd, 0);
+        idOk: EndDialog(Wnd, 1);
+        idCancel: EndDialog(Wnd, 0);
       end;
   end;
 end;
